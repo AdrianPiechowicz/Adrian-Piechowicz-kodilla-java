13fa35f7cfc2349ca461a1b5ddc30a762736c5b7
AdrianPiechowicz <rarowul@gmail.com>
2018-01-15 09:16:20 +0100

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+
+        calculator calc = new calculator();
+        int resultAdd = calc.add(5, 3);
+        int resultSubtract = calc.subtract(5, 3);
+
+
+        if (resultAdd == 8) {
+            System.out.println("5 + 3 = 8    test passed");
+        } else {
+            System.out.println("5 + 3 =/= 8  test failed ");
+        }
+
+
+        if (resultSubtract == 2) {
+            System.out.println("5 - 3 = 2    test passed");
+        } else {
+            System.out.println("5 - 3 =/= 2  test failed ");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing;
+
+public class calculator {
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public calculator(){
+
+    }
+}
