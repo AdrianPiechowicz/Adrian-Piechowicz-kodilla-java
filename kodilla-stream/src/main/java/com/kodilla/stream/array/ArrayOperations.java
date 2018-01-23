f0a03225d122abf6bbb78484247d0dc00b46d843
AdrianPiechowicz <rarowul@gmail.com>
2018-01-23 23:41:52 +0100

    6.6

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,22 @@
+/*package com.kodilla.stream.array;
+
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.*;
+
+public final class ArrayOperations {
+    int[] array = new int[20];
+
+    public ArrayOperations(int[] array) {
+        this.array = array;
+    }
+
+    public void getAverage(int[] numbers){
+        List<Integer> result = IntStream.range(0,numbers.length)
+                .collect(t
+
+
+    }
+}*/
